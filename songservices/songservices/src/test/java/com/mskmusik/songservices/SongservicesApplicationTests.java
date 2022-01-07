d57036c77048f9a1560e55dc9ca25e65910f7ed2
karthikdev0 <95904040+karthikdev0@users.noreply.github.com>
2022-01-07 11:25:23 +0000

    first commit

---
+++ songservices/songservices/src/test/java/com/mskmusik/songservices/SongservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mskmusik.songservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
