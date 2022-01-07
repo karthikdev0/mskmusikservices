60d338440bd5cb26481ceeb67e877068c52ea2d0
karthikdev0 <95904040+karthikdev0@users.noreply.github.com>
2022-01-07 11:23:47 +0000

    first commit

---
+++ songservices/songservices/src/main/java/com/mskmusik/songservices/SongservicesApplication.java
@@ -0,0 +1,13 @@
+package com.mskmusik.songservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongservicesApplication.class, args);
+	}
+
+}
